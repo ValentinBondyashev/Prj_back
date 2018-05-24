14448f0b09e520200df8f4cbaf358ab05929ab42
Kirillpryanikov <Kirillpryanikov>
2018-05-24 10:15:20 +0300

    Added access controll allow origin headers.

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
 app.use(bodyParser.json());
 
+// Add access control allow origin header;
+app.use(function (req, res, next)
+{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+});
+
 // Initialize database;
 global.Sequelize = require('sequelize');
 global.sequelize = new Sequelize('mysql://avadak00_skills:cjxr8b38@avadak00.mysql.tools:3306/avadak00_skills');
